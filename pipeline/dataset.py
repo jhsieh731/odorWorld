8689bef034b76546b1b064d1fce744d5fbe5b9e9
jhsieh731 <jhsieh@college.harvard.edu>
2024-04-09 22:47:35 -0400

    pipieline

--- pipeline/dataset.py
+++ pipeline/dataset.py
@@ -3,10 +3,26 @@ from torch.utils.data import Dataset
 import pickle
 import numpy as np
 
+# default settings are made for Gaussian (most complex)
 class LazyChemDataset(Dataset):
-    def __init__(self, pickle_file, index_file, n_mixture, num_classes):
+    def __init__(
+            self,
+            pickle_file,
+            index_file,
+            n_mixture, # odors per mixture
+            num_classes, # total number of odor classes
+            dset_type=None, # unif, noisy unif, gaussian
+            # n_samples=1, # samples per odor
+            transform=None, # data augmentation: ONLY USE if n_samples > 1
+            target_transform=None,
+        ):
+        self.dset_type = dset_type
+        # self.n_samples = n_samples
+        self.transform = transform
+        self.target_transform = target_transform
         self.pickle_file = pickle_file
         self.num_classes = num_classes
+
         with open(index_file, 'rb') as file:
             self.offsets = pickle.load(file)
         self.n = len(self.offsets)
@@ -31,8 +47,12 @@ class LazyChemDataset(Dataset):
             for id in odor_ids:
                 file.seek(self.offsets[id])
                 chem, label = pickle.load(file)
+                if self.transform:
+                    chem = self.transform(chem)
                 odors.append(chem)
                 y_tensor[label] = 1
+            x_tensor = torch.tensor(sum(odors), dtype=torch.float32)
+            if self.target_transform:
+                y_tensor = self.target_transform(y_tensor)
 
-        x_tensor = torch.tensor(sum(odors), dtype=torch.float32)
         return x_tensor, y_tensor
